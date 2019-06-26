43309cd8fcd9409a630ae076cebc5d7372615bdc
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-06-26 00:31:47 -0500

    Add has_secure_token to user and edit application controller to require login for the most of actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  before_action :require_login
+
+  def require_login
+    authenticate_token || render_unauthorized('Access denied')
+  end
+
+  def current_user
+    @current_user ||= authenticate_token
+  end
+
+  private
+
+  def render_unauthorized(message)
+    errors = { errors: { message: message } }
+    render json: errors, status: :unauthorized
+  end
+
+  def authenticate_token
+    User.find_by_token(cookies.signed[:auth_token])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
-# /app/models/user.rb
+# /app/models/user.rb
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token
+
+  def invalidate_token
+    update(token: nil)
+  end
+
+  def self.valid_login?(email, password)
+    user = find_by(email: email)
+    user if user&.authenticate(password)
+  end
 end
